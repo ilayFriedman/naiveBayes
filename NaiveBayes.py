b306276af975f44c8dd25cd83faa671a2c463f72
ilayFriedman <ilayfri@post.bgu.ac.il>
2019-07-21 17:14:10 +0300

    firstPush!!

---
+++ NaiveBayes.py
@@ -0,0 +1,42 @@
+import csv
+class NaiveBayes:
+    def build(self,folderPath):
+        self.folderPath = folderPath
+
+        #readDATA
+        with open(self.folderPath+'/train.csv', 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            self.linesDataList = list(reader)
+        file.close()
+
+        #readContent
+        with open(self.folderPath + "/Structure.txt", "r") as structureFile:
+            self.content = {}
+            attributes = structureFile.read().replace("\n", "").split("@ATTRIBUTE ")
+            #print(attributes)
+            for att in attributes:
+                start = att.find('{')
+                key = att[:start - 1]
+                values = att[start + 1:-1]
+                values = values.split(",")
+                if (start != -1):
+                    self.content[key] = values
+                else:
+                    start = att.find('NUMERIC')
+                    if (start != -1):
+                        key = att.split(" ")[0]
+                        self.content[key] = 'NUMERIC'
+            #print(self.content)
+        structureFile.close()
+
+    def fillEmptyValues(self):
+
+
+
+
+
+
+
+NB = NaiveBayes()
+
+NB.build("D:/documents/users/ilayfri/Downloads")
