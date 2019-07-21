79f7a4d39cb1acedf9b3fb35abffe4b3422d2f3c
ilayFriedman <ilayfri@post.bgu.ac.il>
2019-07-21 20:40:42 +0300

    firstPush!!!

--- oren.py
+++ oren.py
@@ -2,37 +2,28 @@ import math
 
 from NaiveBayes import NaiveBayes
 
+nb = NaiveBayes()
+nb.build("D:/documents/users/ilayfri/Downloads", 100)
+
+def mEstimator(attIndex, attValue, classIndex,classValue):
+    m = 2
+    #print (m)
+    nc = ncValue(attIndex,attValue,classIndex,classValue)
+   # print ("nc: "+ str(nc))
+    p = 1/float((len(nb.content[nb.attIndexes.keys()[nb.attIndexes.values().index(attIndex)]])))
+    #print ("p: "+ str(p))
+    n = nb.rowByIndex(classIndex).count(classValue)
+    #print ("n: " + str(n))
+
+    return float(float(nc+float(m*p))/float(n+m))
+
+def ncValue(attIndex,attValue,classIndex,classValue):
+    counter = 0
+    for line in nb.train_Data:
+        if(line[attIndex] == attValue and line[classIndex] == classValue):
+            counter += 1
+
+    return counter
 
 
-
-def discretization(bind):
-    nb = NaiveBayes()
-    nb.build("C:/Users/shororen/PycharmProjects/naiveBayes")
-    for att in nb.content.items():
-        if('NUMERIC' in att[1]):
-            print ('###')
-            index = nb.attIndexes[att[0]]
-            nb.train_Data.sort(key=lambda x: x[index])
-            col = map(float, nb.rowByIndex(index))
-            mini = min(col)
-            maxi = max(col)
-            range = (maxi - mini)/bind
-            currVal = round(mini + range,3)
-            j = 1
-            for i in nb.train_Data:
-                # min =
-                tmp = i[index]
-                if(int(i[index]) <= currVal):
-                    i[index] = round(currVal,3)
-                else:
-                    currVal += range
-                    while(int(i[index]) > currVal):
-                        currVal += range
-                    i[index] = round(currVal,3)
-
-    print(nb.train_Data)
-
-
-
-
-discretization(100)
+print (mEstimator(9,"Rural",10,"Y"))
